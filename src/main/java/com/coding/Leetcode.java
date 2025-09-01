288270700fd2ee30ec713b13cb4bb4041f8a8fc7
thirisha19 <thirishamurugesan19@gmail.com>
2025-09-01 11:13:33 +0530

    test commit

--- src/main/java/com/coding/Leetcode.java
+++ src/main/java/com/coding/Leetcode.java
@@ -1,5 +1,5 @@
 class Leetcode{
-    public static void main(int[] nums, int target) {
+    static int[] twoSum(int[] nums, int target){
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
@@ -8,5 +8,13 @@ class Leetcode{
             }
         }
         return new int[]{};
+    }
+    public static void main(String[] args) {
+        int nums[] = {10, 30, 24, 50};
+        int target = 60;
+        int arr[] = twoSum(nums, target);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+ " ");
+        }
     }
 }
